4335afa731235aba61d3668535dd0f9f5783bec3
Zorzan99 <gabriel_zds@hotmail.com>
2023-09-21 19:05:00 -0300

    linkando alguns models com o bd

---
+++ app/Models/Cidade.ts
@@ -0,0 +1,17 @@
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Cidade extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+
+  @column()
+  public nome: string;
+
+  @column()
+  public estado_id: number;
+
+  @column()
+  public ativo: boolean;
+
+}
